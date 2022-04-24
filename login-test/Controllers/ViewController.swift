9786536a8faf146c8739646de67980f7f42d2d57
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-04-24 02:11:26 -0300

    add login page

---
+++ login-test/Controllers/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  login-test
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 23/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var ValidUserMessage: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: UIButton) {
+        let email = Email.text ?? ""
+        let password = Password.text ?? ""
+        
+        var user = User(email, password)
+        
+        setValidUserMessage(validUser: user.isValidUser());
+    }
+    
+    private func setValidUserMessage(validUser: Bool){
+        if(validUser){
+            ValidUserMessage.text = "Valid User"
+            ValidUserMessage.textColor = UIColor.green
+            ValidUserMessage.isHidden = false
+        } else {
+            ValidUserMessage.text = "Invalid User"
+            ValidUserMessage.textColor = UIColor.red
+            ValidUserMessage.isHidden = false
+        }
+    }
+}
---
+++ login-test/Models/User.swift
@@ -0,0 +1,56 @@
+//
+//  User.swift
+//  login-test
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 24/04/22.
+//
+
+import Foundation
+
+struct User {
+    var email: String;
+    var password: String;
+    
+    init(_ email: String, _ password: String) {
+        self.email = email;
+        self.password = password;
+    }
+}
+
+extension User{
+    mutating func isValidUser() -> Bool{
+        var isValid = true
+        
+        if(email.count == 0 || IsValidEmail(email: email)){
+            isValid = false
+        }
+        
+        if(password.count == 0){
+            isValid = false
+        }
+        
+        return isValid;
+    }
+}
+
+private func IsValidEmail(email: String) -> Bool {
+    var isValid = true
+    let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+    
+    do {
+        let regex = try NSRegularExpression(pattern: emailRegEx)
+        let nsString = email as NSString
+        let results = regex.matches(in: email, range: NSRange(location: 0, length: nsString.length))
+        
+        if results.count == 0
+        {
+            isValid = false
+        }
+        
+    } catch let error as NSError {
+        print("Invalid Regex: \(error.localizedDescription)")
+        isValid = false
+    }
+    
+    return isValid
+}
